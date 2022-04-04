63749c037de396e8afa5b4dd1ac5bddbb3e4c6ac
cesar-spaziante <cesar.spaziante@gmail.com>
2022-04-04 19:26:51 -0300

    Getters|TS Install-Compiler

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,9 @@
 import { Negociacao } from "./models/negociacao.js";
-const negociacao = new Negociacao(new Date());
+
+const negociacao = new Negociacao(
+    new Date());
+ 
+  
 console.log(negociacao.volume);
+    
+    
