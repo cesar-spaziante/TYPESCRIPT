2b099310202bd043b164ec85050faea2848ef7c0
cesar-spaziante <cesar.spaziante@gmail.com>
2022-05-17 17:58:12 -0300

    TS - Configuração do Compilador e Watch

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,8 @@
 import { Negociacao } from "./models/negociacao.js";
 
-const negociacao = new Negociacao(
-    new Date());
+const negociacao = new Negociacao(new Date(), 10, 100);
  
   
 console.log(negociacao.volume);
-    
+ 
     
